1c62c8e1ef72314c1b02b336a0a70acebb59ebbd
Hunter Schafer <hschafer@uw.edu>
2023-05-18 00:18:40 -0700

    Make the test client a little more linting friendly
    Still doesn't work

--- tests/client_ideas.py
+++ tests/client_ideas.py
@@ -1,10 +1,13 @@
-from edstem import EdCourse, User, Module, Lesson, Slide, QuizQuestion, Challenge
+from edstem import Challenge, EdCourse, Lesson, Module, QuizQuestion, Slide, User
+
 # Getting information about a course
 course = EdCourse(1234)  # TODO Auth?
 
+id_or_name = ""
+
 ## Get users
 users: list[User] = course.get_all_users()
-user: User = course.get_user(id or name)  # Error if multiple
+user: User = course.get_user(id_or_name)  # Error if multiple
 # How to unify diff user metadata? Merge in the first call?
 # users: list[User2] = course.get_analytics_users()
 
@@ -13,28 +16,28 @@ tutorials: list[str] = course.get_all_tutorials()
 
 ## Get Modules
 modules: list[Module] = course.get_all_modules()
-module: Module = course.get_module(id or name)
-module: Module = Module(id or name)
+module: Module = course.get_module(id_or_name)
+module: Module = Module(id_or_name)
 
 ## Getting Lessons (many ways)
-lessons: list[Lesson]  = course.get_all_lessons()
-lesson: Lesson = Lesson(id or name)
-lesson: Lesson = course.get_lesson(id or name)
-lesson: Lesson = module.get_lesson(id or name)
+lessons: list[Lesson] = course.get_all_lessons()
+lesson: Lesson = Lesson(id_or_name)
+lesson: Lesson = course.get_lesson(id_or_name)
+lesson: Lesson = module.get_lesson(id_or_name)
 
 module_lessons: list[Lesson] = module.get_all_lessons()
 
 ## Get Slide
 slide: Slide = Slide(id)
 slide: Slide = course.get_slide(id)
-slide: Slide = lesson.get_slide(id or name)
+slide: Slide = lesson.get_slide(id_or_name)
 
 # Get Questions
 questions: list[QuizQuestion] = slide.get_questions()
 question: QuizQuestion = questions[0]
 
 # Get Challenges
-challenges: list[Challenge] = course.get_challenge(id or name?)
+challenge: Challenge = course.get_challenge(id_or_name)
 challenge_users: list[User] = challenge.get_users()
 
 # Editing Values
@@ -65,12 +68,12 @@ question.save()
 lesson.get_completions(params)
 
 ## Challenge Results
-lesson.challenge_completions(id or name?, params)
-lesson.challenge_completions(params) # Unsure if possible
+lesson.challenge_completions(id_or_name, params)
+lesson.challenge_completions(params)  # Unsure if possible
 
 ## Quiz Results
-lesson.quiz_completions(id or name?, params)
-lesson.quiz_completions(params) # Unsure if possible
+lesson.quiz_completions(id_or_name, params)
+lesson.quiz_completions(params)  # Unsure if possible
 
 # TBD
 ## Post Grades
